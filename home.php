169f37746ddb9212c479b06b0397eeead6c04e13
AKSHAYA MADHURI <76612327+akshaya-hub@users.noreply.github.com>
2023-05-04 00:36:49 +0530

    Create home.php

---
+++ home.php
@@ -0,0 +1,34 @@
+<?php include 'header.php';?>
+<html>
+    <head>
+        <style>
+            body{
+                background: url("back.jpg");
+                background-size: cover;
+                background-repeat: no-repeat;
+                font-family: 'Poppins', sans-serif;
+            }
+            .container{
+                font-size: 20px;
+                text-align: center;
+                padding: 15px;
+                margin: 20px;
+                color: #fff;
+                line-height: 50px;
+            }
+            marquee{
+                margin-top: 70px;
+                margin-bottom: 0;
+            }
+        </style>
+    </head>
+    <body>
+        <marquee direction="up">
+        <div class="container">
+            <p>Welcome to <h1>INDIAN BANK</h1></p>
+            <img src="bank.jpeg" width="200" height="200">
+            <p>Indian Bank is a major nationalised bank. It is under the ownership of the Ministry of Finance, Government of India. Indian Bank is an Indian Multinational, Public Sector Banking and Financial services statutory body headquartered in Mumbai. The rich heritage and legacy of over 200 years, accredits INDIAN BANK as the most trusted Bank by Indians through generations.</p>
+        </div>
+        </marquee>
+    </body>
+</html>
