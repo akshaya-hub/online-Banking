1c3700058ab3d1f016f5a9cfc0c7add5c234aa5e
AKSHAYA MADHURI <76612327+akshaya-hub@users.noreply.github.com>
2023-05-12 22:04:57 +0530

    Create session_start();.php

---
+++ session_start();.php
@@ -0,0 +1,17 @@
+session_start();
+    if(isset($_POST['Login'])){
+        $email=$_POST['email'];
+        $password=$_POST['password'];
+
+        $query="SELECT * FROM users WHERE `email`='$email' AND `password`='$password'";
+        $result=mysqli_query($conn,$query);
+        $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+
+        if(mysqli_num_rows($result)==1)
+        {
+        $_SESSION['email'] = $login_user;  // Initialising Session
+        header("location: index.php");  // Redirecting to other page
+        }else{
+        $error='Incorrect emailid or password';
+    }
+}
